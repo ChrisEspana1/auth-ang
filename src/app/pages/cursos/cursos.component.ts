5eef97c5d4de2fdab6d195f303d15d408a3f446d
Christian Antonio España Mazariegos <142623061+ChrisEspana1@users.noreply.github.com>
2025-10-10 16:52:04 -0600

    Funcionalidad de filtro de cursos por categoria, nivel o palabra clave en componente curso

--- src/app/pages/cursos/cursos.component.ts
+++ src/app/pages/cursos/cursos.component.ts
@@ -5,30 +5,52 @@ import { Router } from '@angular/router';
 import HomeComponent from '../home/home.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [HomeComponent, CommonModule, HttpClientModule],
+  imports: [HomeComponent, CommonModule, HttpClientModule, FormsModule],
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
   cursos: Curso[] = [];
+  cursosFiltrados: Curso[] = [];
   cursosPaginados: Curso[] = [];
+
+  filtroCategoria = 'todos';
+  filtroNivel = 'todos';
+  filtroTexto = '';
+  
   paginaActual = 1;
   cursosPorPagina = 4;
   paginas: number[] = [];
-
+  
   constructor(private cursoService: CursoService, private router: Router) {}
 
   ngOnInit(): void {
     this.cursoService.getCursos().subscribe(data => {
       this.cursos = data;
       this.generarPaginas();
+      this.aplicarFiltros();
       this.actualizarCursosPaginados();
     });
   }
+  
+aplicarFiltros(): void {
+    this.paginaActual = 1;
+    this.cursosFiltrados = this.cursos.filter(curso => {
+      const coincideCategoria = this.filtroCategoria === 'todos' || curso.categoria === this.filtroCategoria;
+      const coincideNivel = this.filtroNivel === 'todos' || curso.nivel === this.filtroNivel;
+      const coincideTexto = this.filtroTexto.trim() === '' || curso.titulo.toLowerCase().includes(this.filtroTexto.toLowerCase()) || curso.descripcion.toLowerCase().includes(this.filtroTexto.toLowerCase());
+      return coincideCategoria && coincideNivel && coincideTexto;
+    });
+    console.log('Cursos filtrados:', this.cursosFiltrados);
+    this.generarPaginas();
+    this.actualizarCursosPaginados();
+  }
+
 
   generarPaginas(): void {
     const totalPaginas = Math.ceil(this.cursos.length / this.cursosPorPagina);
@@ -38,7 +60,7 @@ export class CursosComponent implements OnInit {
   actualizarCursosPaginados(): void {
     const inicio = (this.paginaActual - 1) * this.cursosPorPagina;
     const fin = inicio + this.cursosPorPagina;
-    this.cursosPaginados = this.cursos.slice(inicio, fin);
+    this.cursosPaginados = this.cursosFiltrados.slice(inicio, fin);
   }
 
   cambiarPagina(nuevaPagina: number): void {
@@ -49,4 +71,6 @@ export class CursosComponent implements OnInit {
   verCurso(id: string): void {
     this.router.navigate(['/curso', id]);
   }
+
+
 }
