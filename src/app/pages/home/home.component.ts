a191a70063e104da0ba5cf3f00dbfaab6ae0380d
Christian Antonio España Mazariegos <142623061+ChrisEspana1@users.noreply.github.com>
2025-10-13 13:49:41 -0600

    unificacion del footer en componente home. Añadido de menu hamburguesa para navegacion en el sitio (sidenav bar - en home component)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,33 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { AuthService } from '../../core/services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
-
+import { AuthService } from '../../core/services/auth.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatButtonToggleModule],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./style.css'],
-  styles: [
-    `
-      mat-toolbar {
-        display: flex;
-        justify-content: space-between;
-      }
-    `,
-  ],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    RouterOutlet
+  ]
 })
 export default class HomeComponent {
-  private _router: Router;
-  private authservice: AuthService;
+  constructor(private _router: Router, private authservice: AuthService) {}
 
-  constructor(router: Router, authService: AuthService) {
-    this._router = router;
-    this.authservice = authService;
+  redirectTo(url: string): void {
+    this._router.navigateByUrl(url);
+  }
+
+  goHome() {
+    this._router.navigateByUrl('/home');
+  }
+
+  redirectToForos() {
+    this._router.navigate(['/foros']);
+  }
+
+  redirectToCursos() {
+    this._router.navigate(['/cursos']);
   }
 
   async logOut(): Promise<void> {
@@ -39,19 +48,10 @@ export default class HomeComponent {
     }
   }
 
-  redirectTo(url: string): void {
-    this._router.navigateByUrl(url);
-  }  
-  redirectToProveedor(){
-    this._router.navigate(['/proveedor']);
-  }
-  redirectToCursos(){
-    this._router.navigate(['/cursos']);
-  }
-  redirectToForos(){
-    this._router.navigate(['/foros']);
-  }
-  goHome(){
-    this._router.navigateByUrl('/home');
+  @ViewChild('sidenav') sidenav: any;
+
+  toggleMenu() {
+    this.sidenav.toggle();
   }
 }
+
