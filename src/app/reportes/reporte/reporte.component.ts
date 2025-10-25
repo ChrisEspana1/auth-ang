08f399ebe9fb36252c0d743a058e80b7be186ef7
Christian Antonio España Mazariegos <142623061+ChrisEspana1@users.noreply.github.com>
2025-10-25 11:39:40 -0600

    Creacion de reporte evolucion-temporal component

--- src/app/reportes/reporte/reporte.component.ts
+++ src/app/reportes/reporte/reporte.component.ts
@@ -3,6 +3,8 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { PromedioCalificacionesComponent } from '../promedio-calificaciones/promedio-calificaciones.component';
 import { RecomendacionesComponent } from '../recomendaciones/recomendaciones.component';
 import { ClaridadVsContenidoComponent } from '../claridad-vs-contenido/claridad-vs-contenido.component';
+import { EvolucionTemporalComponent } from '../evolucion-temporal/evolucion-temporal.component';
+
 
 @Component({
   selector: 'app-reporte',
@@ -11,9 +13,11 @@ import { ClaridadVsContenidoComponent } from '../claridad-vs-contenido/claridad-
     MatTabsModule,
     PromedioCalificacionesComponent,
     RecomendacionesComponent,
-    ClaridadVsContenidoComponent
+    ClaridadVsContenidoComponent,
+    EvolucionTemporalComponent
   ],
   templateUrl: './reporte.component.html',
   styleUrls: ['./reporte.component.css']
 })
 export class ReporteComponent {}
+
